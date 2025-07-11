53119c0b67eee0d7ff87adb8833b7f250cd4d02d
AlexField442 <alexfield290@gmail.com>
2025-07-11 23:45:16 +1000

    Removed original ROM from distribution
    Mainly removed out of potential copyright concerns, so you'll instead have to provide it yourself.

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -3,11 +3,9 @@
 REM // build the ROM
 call build
 
-REM  // run fc against the 1103 dump
-echo -------------------------------------------------------------
-if exist s3built.bin ( fc /b s3built.bin Sonic_3_Proto.md
-) else echo Sega_TMSS.bin does not exist, probably due to an assembly error
+REM // compare built ROM with dumped ROM
+echo ----
+if exist s3built.bin ( fc /b s3built.bin s3original.bin )
 
 REM // if someone ran this from Windows Explorer, prevent the window from disappearing immediately
 pause
-
