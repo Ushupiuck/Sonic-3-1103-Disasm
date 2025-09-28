ea7205778a14783831048cfd454cbddcdb7495c5
Ushupiuck <giancarloknd@hotmail.com>
2025-09-28 05:02:35 -0300

    Update obj_0x7C.asm

--- Data/S1_Obj/obj_0x7C.asm
+++ Data/S1_Obj/obj_0x7C.asm
@@ -1,62 +1,70 @@
-;===============================================================================
-; Objeto 0x7C - Flash do anel gigante usado no Sonic 1 para acesso ao
-; ->>>			Special Stage não usado (Left over)
-;===============================================================================
-; Offset_0x010D26:
-				moveq	#$00, D0
-				move.b	Obj_Routine(A0), D0								 ; $0005
-				move.w	Offset_0x010D34(PC, D0), D1
-				jmp		Offset_0x010D34(PC, D1)
+; ---------------------------------------------------------------------------
+; Object 7C - flash effect when you collect the giant ring
+; Leftover from Sonic 1 (Yes, it has survived THIS long)
+; ---------------------------------------------------------------------------
+; RingFlash:
+		moveq	#0,d0
+		move.b	Obj_Routine(a0),d0
+		move.w	RingFlash_Index(pc,d0),d1
+		jmp	RingFlash_Index(pc,d1)
 ;-------------------------------------------------------------------------------
-Offset_0x010D34:
-				dc.w	Offset_0x010D3A-Offset_0x010D34
-				dc.w	Offset_0x010D64-Offset_0x010D34
-				dc.w	Offset_0x010DD6-Offset_0x010D34
+RingFlash_Index:
+		dc.w	Flash_Main-RingFlash_Index
+		dc.w	Flash_ChkDel-RingFlash_Index
+		dc.w	Flash_Delete-RingFlash_Index
 ;-------------------------------------------------------------------------------
-Offset_0x010D3A:
-				addq.b	#$02, Obj_Routine(A0)							 ; $0005
-				move.l	#Big_Ring_Flash_Mappings, Obj_Map(A0) ; Offset_0x010EFA,$000C
-				move.w	#$2462, Obj_Art_VRAM(A0)						 ; $000A
-				ori.b	#$04, Obj_Flags(A0)								 ; $0004
-				move.w	#$0000, Obj_Priority(A0)						 ; $0008
-				move.b	#$20, Obj_Width(A0)								 ; $0007
-				move.b	#$FF, Obj_Map_Id(A0)							 ; $0022
+
+Flash_Main:	; Routine 0
+		addq.b	#2,Obj_Routine(a0)
+		move.l	#Big_Ring_Flash_Mappings,Obj_Map(a0)
+		move.w	#$2462,Obj_Art_VRAM(a0)
+		ori.b	#4,Obj_Flags(a0)
+		move.w	#0,Obj_Priority(a0)
+		move.b	#$20,Obj_Width(a0)
+		move.b	#$FF,Obj_Map_Id(a0)
+
+Flash_ChkDel:	; Routine 2
+		bsr.s	Flash_Collect
+;		for reference sake, the code below is essentially
+;		out_of_range.w	DeleteObject
+		move.w	Obj_X(a0),d0
+		andi.w	#$FF80,d0
+		sub.w	(Camera_X_Left).w,d0
+		cmpi.w	#$280,d0
+		bhi.w	DeleteObject
+		bra.w	DisplaySprite
+
 ;-------------------------------------------------------------------------------
-Offset_0x010D64:
-				bsr.s	Offset_0x010D7E
-				move.w	Obj_X(A0), D0									 ; $0010
-				andi.w	#$FF80, D0
-				sub.w	(Camera_X_Left).w, D0						 ; $FFFFF7DA
-				cmpi.w	#$0280, D0
-				bhi.w	DeleteObject						   ; Offset_0x011138
-				bra.w	DisplaySprite						   ; Offset_0x011148
-Offset_0x010D7E:
-				subq.b	#$01, Obj_Ani_Time(A0)							 ; $0024
-				bpl.s	Offset_0x010DC4
-				move.b	#$01, Obj_Ani_Time(A0)							 ; $0024
-				addq.b	#$01, Obj_Map_Id(A0)							 ; $0022
-				cmpi.b	#$08, Obj_Map_Id(A0)							 ; $0022
-				bcc.s	Offset_0x010DC6
-				cmpi.b	#$03, Obj_Map_Id(A0)							 ; $0022
-				bne.s	Offset_0x010DC4
-				move.l	Obj_Control_Var_10(A0), A1						 ; $0040
-				move.b	#$06, Obj_Routine(A1)							 ; $0005
-				move.b	#$1C, (Obj_Player_One+Obj_Ani_Number).w		 ; $FFFFB020
-				move.b	#$01, (Special_Stage_Entry_Flag).w			 ; $FFFFF7CD
-				lea		(Obj_Player_One).w, A1						 ; $FFFFB000
-				bclr	#$01, Obj_Player_Status(A1)						 ; $002F
-				bclr	#$00, Obj_Player_Status(A1)						 ; $002F
-Offset_0x010DC4:
-				rts
-Offset_0x010DC6:
-				addq.b	#$02, Obj_Routine(A0)							 ; $0005
-				move.l	#$00000000, (Obj_Player_One).w				 ; $FFFFB000
-				addq.l	#$04, A7
-				rts
+
+Flash_Collect:
+		subq.b	#1,Obj_Ani_Time(a0)
+		bpl.s	locret_10DC4
+		move.b	#1,Obj_Ani_Time(a0)
+		addq.b	#1,Obj_Map_Id(a0)
+		cmpi.b	#8,Obj_Map_Id(a0)	; has animation finished?
+		bcc.s	Flash_End		; if yes, branch
+		cmpi.b	#3,Obj_Map_Id(a0)	; is 3rd frame displayed?
+		bne.s	locret_10DC4		; if not, branch
+		move.l	Obj_Control_Var_10(a0),a1	; get parent object address
+		move.b	#6,Obj_Routine(a1)		; delete parent object
+		move.b	#$1C,(Obj_Player_One+Obj_Ani_Number).w	; make Player invisible
+		move.b	#1,(Special_Stage_Entry_Flag).w		; stop Player getting bonuses
+		lea	(Obj_Player_One).w,a1
+		bclr	#1,Obj_Player_Status(a1)	; remove invincibility
+		bclr	#0,Obj_Player_Status(a1)	; remove shield
+
+locret_10DC4:
+		rts
 ;-------------------------------------------------------------------------------
-Offset_0x010DD6:
-				bra.w	DeleteObject						   ; Offset_0x011138
-;===============================================================================
-; Objeto 0x7C - Flash do anel gigante usado no Sonic 1 para acesso ao
-; <<<-			Special Stage não usado (Left over)
-;===============================================================================
+
+Flash_End:
+		addq.b	#2,Obj_Routine(a0)
+		move.l	#0,(Obj_Player_One).w	; remove Player object
+		addq.l	#4,sp
+		rts
+; End of function Flash_Collect
+
+;-------------------------------------------------------------------------------
+
+Flash_Delete:	; Routine 4
+		bra.w	DeleteObject
