1d04fb7dfa28b9319e54ef21a2ced69229ed4dfa
Ushupiuck <giancarloknd@hotmail.com>
2025-10-04 20:44:26 -0300

    Update obj_0xDC.asm

--- Data/S2_Obj/obj_0xDC.asm
+++ Data/S2_Obj/obj_0xDC.asm
@@ -1,56 +1,57 @@
-;===============================================================================
-; Objeto 0xDC - Anéis usados como prêmio nos caça níqueis na Casino Night
-; ->>>
-;===============================================================================
-; Offset_0x010FC2:
-		moveq	#0,D0
-		move.b	Obj_Routine(A0),D0								 ; $0005
-		move.w	Offset_0x010FD0(PC,D0.w),D1
-		jmp	Offset_0x010FD0(PC,D1.w)
-;-------------------------------------------------------------------------------
-Offset_0x010FD0:
-		dc.w	Offset_0x010FD6-Offset_0x010FD0
-		dc.w	Offset_0x01102A-Offset_0x010FD0
-		dc.w	Offset_0x011036-Offset_0x010FD0
-;-------------------------------------------------------------------------------
-Offset_0x010FD6:
-		moveq	#0,D1
-		move.w	Obj_Control_Var_0C(A0),D1						 ; $003C
-		swap	D1
-		move.l	Obj_Control_Var_04(A0),D0						 ; $0034
-		sub.l	D1,D0
-		asr.l	#4,D0
-		sub.l	D0,Obj_Control_Var_04(A0)						 ; $0034
-		move.w	Obj_Control_Var_04(A0),Obj_X(A0)		  ; $0034, $0010
-		moveq	#0,D1
-		move.w	Obj_Control_Var_0E(A0),D1						 ; $003E
-		swap	D1
-		move.l	Obj_Control_Var_08(A0),D0						 ; $0038
-		sub.l	D1,D0
-		asr.l	#4,D0
-		sub.l	D0,Obj_Control_Var_08(A0)						 ; $0038
-		move.w	Obj_Control_Var_08(A0),Obj_Y(A0)		  ; $0038, $0014
-		lea	Slot_Machine_Rings_Animate_Data(PC),A1 ; Offset_0x01103A
-		bsr.w	AnimateSprite						   ; Offset_0x01115E
-		subq.w	#1,Obj_Control_Var_10(A0)					 ; $0040
-		bne.w	DisplaySprite						   ; Offset_0x011148
-		move.l	Obj_Timer(A0),A1								 ; $002E
-		subq.w	#1,(A1)
-		bsr.w	CollectRing			   ; Offset_0x010A20
-		addi.b	#2,Obj_Routine(A0)							 ; $0005
-;-------------------------------------------------------------------------------
-Offset_0x01102A:
-		lea	Rings_Animate_Data(PC),A1			   ; Offset_0x010DDA
-		bsr.w	AnimateSprite						   ; Offset_0x01115E
-		bra.w	DisplaySprite						   ; Offset_0x011148
-Offset_0x011036:
-		bra.w	DeleteObject						   ; Offset_0x011138
-;-------------------------------------------------------------------------------
-Slot_Machine_Rings_Animate_Data:							   ; Offset_0x01103A
+; ----------------------------------------------------------------------------
+; Object DC - Ring prize from Casino Night Zone
+; ----------------------------------------------------------------------------
+; casino_prize_x_pos =		objoff_30	; X position of the ring with greater precision
+; casino_prize_y_pos =		objoff_34	; Y position of the ring with greater precision
+; casino_prize_machine_x_pos =	objoff_38	; X position of the slot machine that generated the ring
+; casino_prize_machine_y_pos =	objoff_3A	; Y position of the slot machine that generated the ring
+; casino_prize_display_delay =	objoff_3C	; number of frames before which the ring is displayed
+; ObjDC:
+		moveq	#0,d0
+		move.b	Obj_Routine(a0),d0
+		move.w	ObjDC_Index(pc,d0.w),d1
+		jmp	ObjDC_Index(pc,d1.w)
+; ===========================================================================
+ObjDC_Index:
+		dc.w	ObjDC_Main-ObjDC_Index
+		dc.w	ObjDC_Animate-ObjDC_Index
+		dc.w	ObjDC_Delete-ObjDC_Index
+; ===========================================================================
+ObjDC_Main:
+		moveq	#0,d1
+		move.w	Obj_Control_Var_0C(a0),d1
+		swap	d1
+		move.l	Obj_Control_Var_04(a0),d0
+		sub.l	d1,d0
+		asr.l	#4,d0
+		sub.l	d0,Obj_Control_Var_04(a0)
+		move.w	Obj_Control_Var_04(a0),Obj_X(a0)
+		moveq	#0,d1
+		move.w	Obj_Control_Var_0E(a0),d1
+		swap	d1
+		move.l	Obj_Control_Var_08(a0),d0
+		sub.l	d1,d0
+		asr.l	#4,d0
+		sub.l	d0,Obj_Control_Var_08(a0)
+		move.w	Obj_Control_Var_08(a0),Obj_Y(a0)
+		lea	Slot_Machine_Rings_Animate_Data(pc),a1
+		bsr.w	AnimateSprite
+		subq.w	#1,Obj_Control_Var_10(a0)
+		bne.w	DisplaySprite
+		move.l	Obj_Timer(a0),a1
+		subq.w	#1,(a1)
+		bsr.w	CollectRing
+		addi.b	#2,Obj_Routine(a0)
+; ---------------------------------------------------------------------------
+ObjDC_Animate:
+		lea	Rings_Animate_Data(pc),a1
+		bsr.w	AnimateSprite
+		bra.w	DisplaySprite
+; ===========================================================================
+ObjDC_Delete:
+		bra.w	DeleteObject
+; ===========================================================================
+Slot_Machine_Rings_Animate_Data:
 		dc.w	Offset_0x01103C-Slot_Machine_Rings_Animate_Data
 Offset_0x01103C:
 		dc.b	$01, $00, $01, $02, $03, $FF
-;===============================================================================
-; Objeto 0xDC - Anéis usados como prêmio nos caça níqueis na Casino Night
-; <<<-
-;===============================================================================
